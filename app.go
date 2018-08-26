94a893a9fd554a7f98c9c870790f67f72bc0ca8a
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-26 15:10:26 +1000

    Add convertStringToFloat64 function and take command line arguments

--- app.go
+++ app.go
@@ -3,12 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
-	
+	"strconv"
 )
 
-func main() {
-	args := os.Args
-	annualSalary := args[1]
+/*
+*	Convert string to float64
+*/
+func convertStringToFloat64(str string) float64 {
+	if result, err := strconv.ParseFloat(str, 64); err == nil {
+		return result
+	}
+	return 0
+}
 
-	fmt.Println("Hello"+annualSalary)
+func main() {
+	// Get command line arguments
+	args := os.Args
+
+	// Use first argument as annual salary and convert to float64
+	annualSalary := convertStringToFloat64(args[1])
+
+	// Output annual salary
+	fmt.Println("The annual salary is: $"+fmt.Sprintf("%f", annualSalary))
 }
