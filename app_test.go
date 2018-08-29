1e4b1b572bf451034578978f386dd809bc6a1e23
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-30 09:22:42 +1000

    Add Test_convertStringToFloat64_DataType_WithoutReflect

--- app_test.go
+++ app_test.go
@@ -51,3 +51,26 @@ func Test_convertStringToFloat64_DataType(t *testing.T) {
 		})
 	}
 }
+
+func Test_convertStringToFloat64_DataType_WithoutReflect(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Convert 1.0 to Float", args{"1.0"}, "float64"},
+		{"Convert 2.10 to Float", args{"2.10"}, "float64"},
+		{"Convert 3.14159 to Float", args{"3.14159"}, "float64"},
+		{"Convert a letter", args{"a"}, "float64"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := convertStringToFloat64(tt.args.str); fmt.Sprintf("%T", got) != tt.want {
+				t.Errorf("convertStringToFloat64() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
