3fbdbd77565605d6044c2c74197c1f72ff8160b9
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-31 18:50:10 +1000

    Add test for determineTaxBracket

--- app_test.go
+++ app_test.go
@@ -74,3 +74,36 @@ func Test_convertStringToFloat64_DataType_WithoutReflect(t *testing.T) {
 		})
 	}
 }
+
+func Test_determineTaxBracket(t *testing.T) {
+	type args struct {
+		salary float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Zero", args{0}, "None"},
+		{"Less than Zero", args{-1}, "None"},
+		{"Less than 18200", args{18199.0}, "Tax Bracket 1"},
+		{"Equals 18200", args{18200.0}, "Tax Bracket 1"},
+		{"Greater than 18200", args{18201.0}, "Tax Bracket 2"},
+		{"Less than 37000", args{36999.0}, "Tax Bracket 2"},
+		{"Equals 37000", args{37000.0}, "Tax Bracket 2"},
+		{"Greater than 37000", args{37001.0}, "Tax Bracket 3"},
+		{"Less than 90000", args{89000.0}, "Tax Bracket 3"},
+		{"Equals 90000", args{90000.0}, "Tax Bracket 3"},
+		{"Greater than 90000", args{90001.0}, "Tax Bracket 4"},
+		{"Less than 180000", args{179000.0}, "Tax Bracket 4"},
+		{"Equals 180000", args{180000.0}, "Tax Bracket 4"},
+		{"Greater than 180000", args{180001.0}, "Tax Bracket 5"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := determineTaxBracket(tt.args.salary); got != tt.want {
+				t.Errorf("determineTaxBracket() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
